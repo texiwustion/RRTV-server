7149d2d564854e89aaea1fc7efc5dbb7d85a01d1
texiwustion <w6666s@outlook.com>
2023-06-27 15:12:38 +0800

    [bugfix] fix some bugs

--- src/handler/extract-component.ts
+++ src/handler/extract-component.ts
@@ -1,34 +1,47 @@
 import * as node from "vscode-languageserver/node";
 
-import {documents, connection} from '../server'
+import { documents, connection } from '../server'
 
 import { BaseHandler, ContinuousOutputHandler } from "../interface/Handler";
 
-const { textToAst, dataGenerator, modifier } = require('./utils-extract-component'); 
+const { textToAst, dataGenerator, modifier } = require('./utils-extract-component');
 
 export class ExtractComponentHandler extends ContinuousOutputHandler<node.CodeAction[], node.CodeActionParams> {
     concreteHandle(prevOutput: node.CodeAction[], request: node.CodeActionParams): node.CodeAction[] {
         const document = documents.get(request.textDocument.uri)
         const text = document.getText(request.range)
         const ast = textToAst(text)
+        if (ast === 6) {
+            return [...prevOutput]
+        }
         const { valid, specified } = dataGenerator(ast)
         if (!valid) {
             return [...prevOutput]
         }
 
 
-        const codeAction:node.CodeAction = {
+        const codeAction: node.CodeAction = {
             title: "Extract-Component",
-            kind:   node.CodeActionKind.RefactorExtract,
+            kind: node.CodeActionKind.RefactorExtract,
             data: document.uri
         }
 
-        const {newText, _range} = modifier(ast, specified)
-       
+        const { newText, _range } = modifier(ast, specified)
+        const newRange = {
+            start: {
+                line: request.range.start.line + _range.start.line,
+                character: request.range.start.character + _range.start.column
+            },
+            end: {
+                line: request.range.start.line + _range.end.line,
+                character: request.range.start.character + _range.end.column
+            }
+        }
+
         const change: node.WorkspaceChange = new node.WorkspaceChange()
         const a = change.getTextEditChange(document)
         connection.window.showInformationMessage(newText);
-        a.replace(request.range, newText)
+        a.replace(newRange, newText)
         codeAction.edit = change.edit
         connection.window.showInformationMessage(JSON.stringify(codeAction));
         return [...prevOutput, codeAction]
